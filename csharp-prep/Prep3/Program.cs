72179ba02c494ccdc925ea4067162791bef36d88
Froylan Sanchez <san24112@byui.edu>
2025-11-03 11:48:49 -0700

    Preps 3 to 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string mn_text = Console.ReadLine();
+        int magic_number = int.Parse(mn_text);
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guess_number = int.Parse(guess);
+        } while (guess_number = magic_number);
+        if 
+
+
     }
 }
